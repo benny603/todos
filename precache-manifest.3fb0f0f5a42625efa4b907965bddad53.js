2b5ade2cdbeffbd8785f2d6ad1c56769e9efe7ce
Benny Wu <bennywu603@gmail.com>
2019-11-30 21:25:21 +0800

    Updates

---
+++ precache-manifest.3fb0f0f5a42625efa4b907965bddad53.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78689459ada11ad0cca2561931eb044b",
+    "url": "/todos/index.html"
+  },
+  {
+    "revision": "5444b571cda8a99ebd87",
+    "url": "/todos/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "e1a5ebcdc5c024b07498",
+    "url": "/todos/static/js/2.ad3b01b9.chunk.js"
+  },
+  {
+    "revision": "5444b571cda8a99ebd87",
+    "url": "/todos/static/js/main.a660ade0.chunk.js"
+  },
+  {
+    "revision": "071d016f0c66a7e68fd9",
+    "url": "/todos/static/js/runtime-main.73fa602b.js"
+  }
+]);
